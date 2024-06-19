4d095d014d348bf6dfa0ff5b60ec7a52cc5ec610
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-06-19 20:11:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import './App.css';
+import Experience from './components/Experience/Experience';
+import HomePage from './components/HomePage/HomePage.jsx';
+ import Navbar from './components/Navbar/Navbar';
+ import Projects from './components/Projects/Projects';
+ import TechStack from './components/TechStack/TechStack';
+
+
+function App() {
+
+//Portfolio website
+
+// 1.Navbar
+//2. HomePage
+//3. Tech Stack
+//4. Experience
+//5. Projects
+
+  return (
+    <div className='app'>
+   <Navbar/>
+   <HomePage/>
+   <TechStack/>
+   <Experience/>
+   <Projects/>
+  
+
+
+
+      
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+//import { NAVBAR_TITLE, navOptions } from "../../constants";
+ import "./Navbar.css";
+
+
+// function NavOption(props) {
+//   return <p>{props.navItem}</p>;
+// }
+
+function Navbar() {
+
+  return (
+    <div className='navbar_container'>
+      <div className='navbar_left'>sc</div>
+      <div className='navbar_right'>
+        <p> About</p>
+        <p>Tools</p>
+        <p>Experience</p>
+        <p>Projects</p>
+        <p>Resume</p>
+      </div>
+      
+      </div>
+  )
+}
+
+export default Navbar;
